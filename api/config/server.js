472538a29903d39baf345d1b83ea6ffea2ab260e
cestle18 <cestle18@users.noreply.github.com>
2020-12-08 16:28:18 +0100

    my movie aoo

---
+++ api/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '7623e0318245ce920e1f29a5e596c435'),
+    },
+  },
+});
---
+++ api/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'b5f44add-26a9-465e-9c0b-97d40385668e'
+};
